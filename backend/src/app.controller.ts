259ea56d94a24ccd38aa107d814aaa1d94cb2477
Jesus Yacolca <jesus.yacolca17@gmail.com>
2023-08-24 21:39:32 -0500

    Update API route

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CommitModel } from './app.interface';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
