152c355c8f8c69afa9d515ed0400473300318719
Jesus Yacolca <jesus.yacolca17@gmail.com>
2023-08-24 21:18:43 -0500

    Update tests

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "API Index"', () => {
+      expect(appController.getIndex()).toBe('API Index');
     });
   });
 });
