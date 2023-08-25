50aa12b4cbdc271e51a2bb3014fd341e2e0c2d2d
Jesus Yacolca <jesus.yacolca17@gmail.com>
2023-08-24 21:15:53 -0500

    Add GET Commit service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom, catchError } from 'rxjs';
+import { CommitModel } from './app.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService){}
+
+  getIndex(): string {
+    return 'API Index';
+  }
+
+  async getCommits(): Promise<CommitModel[]> {
+    const url = 'https://api.github.com/repos/jesusmiguel123/Test/commits';
+    const res = this.httpService.get(url).pipe(
+      catchError((error) => {
+        throw new HttpException({
+          statusCode: error.response.status,
+          message: error.response.statusText
+        }, error.response.status)
+      })
+    );
+    const { data } = await firstValueFrom(res);
+    const response = data.map(({ sha, commit }) => ({
+      id: sha,
+      author: commit?.author,
+      commiter: commit?.committer,
+      message: commit?.message
+    }));
+    return response;
   }
 }
